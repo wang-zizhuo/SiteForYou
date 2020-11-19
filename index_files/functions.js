a6654419a09c4f430f391ef742385d088b141f3e
wang-zizhuo <conorlu@163.com>
2020-11-19 22:31:06 +0800

    第二次提交，时间设定11/19,22：35，测试ios系统

--- index_files/functions.js
+++ index_files/functions.js
@@ -39,8 +39,29 @@ $(window).resize(function() {
 
 function timeElapse(date){
 	var seconds = (+ new Date() - date.getTime()) / 1000;
+	if(isIOS()){
+		seconds += 8*60*60; //补偿ios系统的8小时时差
+	}
+	console.log(parseInt(seconds))
+	if(parseInt(seconds) == 31622400){
+		window.location.reload(true);
+	}
 	var days = Math.floor(seconds / (3600 * 24));
+	if(days >= 366){
+		days  = days % 366;
+		config.names[0] = "23岁";
+		config.lines = [
+	        "子卓",
+	        "花开一季，有落红为证",
+	        "逝去的岁月，如何能托于烟尘",
+	        "你的浅笑，是我永生眷恋",
+	        "相思的真情，是否能明白一二",
+	        "亲爱的宝贝，你是我的唯一",
+	        "生日快乐。。。",
+	    ];
+	}
 	seconds = seconds % (3600 * 24);
+
 	var hours = Math.floor(seconds / 3600);
 	if (days >= 0)
 	{
@@ -55,7 +76,12 @@ function timeElapse(date){
 		seconds = Math.floor(seconds % 60);
 		seconds = (seconds + "").replace(/\.\d*/, "");
 		var result = " <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
-		$("#clock").html(result);	
+		$("#clock").html(result);
 	}
 	
+}
+
+function isIOS(){
+	let u = navigator.userAgent;
+	let isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); 
 }
