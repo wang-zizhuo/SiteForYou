89c197c8045646b0243cfefeebf0d609b24f457c
wang-zizhuo <conorlu@163.com>
2020-11-22 17:22:49 +0800

    修复ios的两秒时间差，修改了configs.lines内容

--- index_files/functions.js
+++ index_files/functions.js
@@ -40,7 +40,7 @@ $(window).resize(function() {
 function timeElapse(date){
 	var seconds = (+ new Date() - date.getTime()) / 1000;
 	if(isIOS()){
-		seconds += 8*60*60; //补偿ios系统的8小时时差
+		seconds += 8*60*60 + 2; //补偿ios系统的8小时时差，测试之后发现差了2秒，这里补上
 	}
 	if(parseInt(seconds) == 31622400){
 		window.location.reload(true);
@@ -50,13 +50,16 @@ function timeElapse(date){
 		days  = days % 366;
 		config.names[0] = "23岁";
 		config.lines = [
-	        "子卓",
-	        "22岁马上，有落红为证",
-	        "逝去的岁月，如何能托于烟尘",
-	        "你的浅笑，是我永生眷恋",
-	        "相思的真情，是否能明白一二",
-	        "亲爱的宝贝，你是我的唯一",
-	        "生日快乐。。。",
+	        "子卓~",
+	        "廿二岁末 廿三岁初",
+	        "成长不期而遇 生日从不逾期",
+	        "美好的事物一定会在新的一年满怀惊喜",
+	        "每一岁都奔走在自己的热爱里 按时长大",
+	        "成为自己喜欢的样子 心中所想如愿以偿",
+	        "历世事而不失少年意趣 保守坚定与热情",
+	        "依然能够为世间那些真心实意而心动",
+	        "不哭泣 不沮丧 有自由 有光芒",
+	        "All the best on your special day!",
 	    ];
 	}
 	seconds = seconds % (3600 * 24);
